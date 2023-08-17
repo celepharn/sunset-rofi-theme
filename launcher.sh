f6508e669cf55dbe4d161fe497fca6aa99996383
Celepharn <65320339+celepharn@users.noreply.github.com>
2023-08-16 22:01:41 -0600

    Added launcher.sh

---
+++ launcher.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Hello! Hope I am making this clear enough so you can modify it to your needs
+
+# -------- About me --------- #
+# author = Emiliano Samario   #
+# github username = celepharn #
+# mail = celephrn@hotmail.com #
+# --------------------------- #
+
+file = "$HOME/.config/scripts/rofi/colors.rasi"
+
+rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/scripts/rofi/launcher.rasi
